64f1d0abd7ccbeaa1f8f20da3bd20ecc673378e3
Bjorniavelli <games@bjorniavelli.com>
2015-01-07 01:53:34 -0600

    Broken.  Remove most code and begin a better hierarchy of data, starting with the page and the tabs.

--- Pixpls.js
+++ Pixpls.js
@@ -1,246 +1,269 @@
-function Generator (params) {//name, costRatio, produceTarget, produce) {
-  this.name = params.name;
-  this.num = 0;
-  this.costRatio = params.costRatio;
-  this.produceTarget = params.produceTarget;
-  this.produce = params.produce;
-  this.el = $("<li />");
-}
-Generator.prototype = {
-  parent: Generators,
+// function Generator (params) {//name, costRatio, produceTarget, produce) {
+//   this.name = params.name;
+//   this.num = 0;
+//   this.costRatio = params.costRatio;
+//   this.produceTarget = params.produceTarget;
+//   this.produce = params.produce;
+//   this.el = $("<li />");
+// }
+// Generator.prototype = {
+//   parent: Generators,
+//
+//   render: function() {
+//     this.renderArticle();
+//     this.renderMenu();
+//   },
+//
+//   renderMenu: function() {
+//     var t = this;
+//
+//     this.el.html("<p><a href=\"\">" + this.name + "</a>: " + this.num + "</p>");
+//     this.el.find("p a").click(function(e) {
+//       e.preventDefault();
+//
+//       //This so totally breaks encapsulation... >_>
+//       $("article").replaceWith(t.articleEl);
+//     });
+//   },
+//
+//   renderArticle: function() {
+//     this.articleEl = $("<article />");
+//     this.articleEl.append("<h1>" + this.name + "</h1>");
+//     this.articleEl.append("<h2>Owned: " + this.num + "</h2>");
+//     // Need to update this so that it outputs the current production rate per this.num
+//     this.articleEl.append("<h3>Produces " + (this.produce || "nothing") +
+//       (this.produce && this.produceTarget ? (" " + Generators[this.produceTarget].name) : "") + "</h3>");
+//   }
+// }
+// var Generators = {
+//   click: new Generator({
+//     name: "Click"
+//   }),
+//   pixel: new Generator({
+//     name: "Pixel",
+//     costRatio: 1.1,
+//     produceTarget: "click",
+//     produce: -1
+//   }),
+//   renderer: new Generator({
+//     name: "Renderer",
+//     num: 0,
+//     costRatio: 2,
+//     produceTarget: "pixel",
+//     produce: 1
+//   }),
+//   extruder: new Generator({
+//     name: "Extruder",
+//     costRatio: 2,
+//     produceTarget: "renderer",
+//     produce: 1
+//   }),
+//   electronicskit: new Generator({
+//     name: "Electronics Kit",
+//     costRatio: 2,
+//     produceTarget: "extruder",
+//     produce: 1
+//   }),
+//   factory: new Generator({
+//     name: "Factory",
+//     costRatio: 2,
+//     produceTarget: "electronicskit",
+//     produce: 1
+//   }),
+//   cementprinter: new Generator({
+//     name: "Cement Printer",
+//     costRatio: 2,
+//     produceTarget: "factory",
+//     produce: 1
+//   }),
+//   designlab: new Generator({
+//     name: "Design Lab",
+//     costRatio: 2,
+//     produceTarget: "cementprinter",
+//     produce: 1
+//   }),
+//   ai: new Generator({
+//     name: "AI",
+//     costRatio: 2,
+//     produceTarget: "designlab",
+//     produce: 1
+//   })
+// }
+//
+// function Tab (params) {
+//   this.id = params.id;
+//   this.name = params.name;
+//   this.menu = params.menu;
+//   this.enabled = params.enabled;
+// };
+// // Tabs render to the <section>
+// // render should render the menu option and... create a section that may or may not be displayed?
+// // renderMenu render's each item and then adds it to the list
+// Tab.prototype = {
+//   Tab.
+//
+//
+//   render: function() {
+//     this.renderNav();
+//     this.renderSection();
+//   },
+//
+//   renderNav: function() {
+//     var t = this;
+//     this.navEl = $("<li />");
+//     this.navEl.html("<a href=\"\">" + this.name + "</a>");
+//     this.navEl.click(function(e) {
+//       e.preventDefault();
+//       $("section").replaceWith(t.sectionEl);
+//     });
+//     $("nav").append(this.navEl);
+//   },
+//
+//   renderSection: function() {
+//     this.sectionEl = $("<section />");
+//
+//     this.renderMenu();
+//     this.sectionEl.append(this.menuEl);
+//
+//     this.emptyArticleEl = $("<article />");
+//     this.articleEl = this.emptyArticleEl;
+//     this.sectionEl.append(this.articleEl);
+//   },
+//
+//   renderMenu: function() {
+//     this.menuEl = $("<menu />");
+//
+//     for (menuItem in this.menu) {
+//       var m = this.menu[menuItem];
+//       m.render();
+//       this.menuEl.append(m.el);
+//     }
+//   },
+//
+//   enable: function() {
+//     this.enabled = true;
+//   }
+// };
+//
+//
 
-  render: function() {
-    this.renderArticle();
-    this.renderMenu();
+var TabsData = {
+  generators: {
+    name: "Generators",
+//    menu: Generators,
+    enabled: true,
   },
-
-  renderMenu: function() {
-    var t = this;
-
-    this.el.html("<p><a href=\"\">" + this.name + "</a>: " + this.num + "</p>");
-    this.el.find("p a").click(function(e) {
-      e.preventDefault();
-
-      //This so totally breaks encapsulation... >_>
-      $("article").replaceWith(t.articleEl);
-    });
+  hero: {
+    name: "Hero",
+    enabled: false
   },
-
-  renderArticle: function() {
-    this.articleEl = $("<article />");
-    this.articleEl.append("<h1>" + this.name + "</h1>");
-    this.articleEl.append("<h2>Owned: " + this.num + "</h2>");
-    // Need to update this so that it outputs the current production rate per this.num
-    this.articleEl.append("<h3>Produces " + (this.produce || "nothing") +
-      (this.produce && this.produceTarget ? (" " + Generators[this.produceTarget].name) : "") + "</h3>");
+  crafting: {
+    name: "Crafting",
+    enabled: false
+  },
+  camp: {
+    name: "Camp",
+    enabled: false
+  },
+  help: {
+    name: "Help",
+    enabled: true
+  },
+  settings: {
+    name: "Settings",
+    enabled: true
   }
-}
-var Generators = {
-  click: new Generator({
-    name: "Click"
-  }),
-  pixel: new Generator({
-    name: "Pixel",
-    costRatio: 1.1,
-    produceTarget: "click",
-    produce: -1
-  }),
-  renderer: new Generator({
-    name: "Renderer",
-    num: 0,
-    costRatio: 2,
-    produceTarget: "pixel",
-    produce: 1
-  }),
-  extruder: new Generator({
-    name: "Extruder",
-    costRatio: 2,
-    produceTarget: "renderer",
-    produce: 1
-  }),
-  electronicskit: new Generator({
-    name: "Electronics Kit",
-    costRatio: 2,
-    produceTarget: "extruder",
-    produce: 1
-  }),
-  factory: new Generator({
-    name: "Factory",
-    costRatio: 2,
-    produceTarget: "electronicskit",
-    produce: 1
-  }),
-  cementprinter: new Generator({
-    name: "Cement Printer",
-    costRatio: 2,
-    produceTarget: "factory",
-    produce: 1
-  }),
-  designlab: new Generator({
-    name: "Design Lab",
-    costRatio: 2,
-    produceTarget: "cementprinter",
-    produce: 1
-  }),
-  ai: new Generator({
-    name: "AI",
-    costRatio: 2,
-    produceTarget: "designlab",
-    produce: 1
-  })
-}
-
-function Tab (params) {
-  this.id = params.id;
-  this.name = params.name;
-  this.menu = params.menu;
-  this.enabled = params.enabled;
-};
-// Tabs render to the <section>
-// render should render the menu option and... create a section that may or may not be displayed?
-// renderMenu render's each item and then adds it to the list
-Tab.prototype = {
-  render: function() {
-    this.renderNav();
-    this.renderSection();
-  },
-
-  renderNav: function() {
-    var t = this;
-    this.navEl = $("<li />");
-    this.navEl.html("<a href=\"\">" + this.name + "</a>");
-    this.navEl.click(function(e) {
-      e.preventDefault();
-      $("section").replaceWith(t.sectionEl);
-    });
-    $("nav").append(this.navEl);
-  },
-
-  renderSection: function() {
-    this.sectionEl = $("<section />");
-
-    this.renderMenu();
-    this.sectionEl.append(this.menuEl);
-
-    this.emptyArticleEl = $("<article />");
-    this.articleEl = this.emptyArticleEl;
-    this.sectionEl.append(this.articleEl);
-  },
-
-  renderMenu: function() {
-    this.menuEl = $("<menu />");
-
-    for (menuItem in this.menu) {
-      var m = this.menu[menuItem];
-      m.render();
-      this.menuEl.append(m.el);
-    }
-  },
-
-  enable: function() {
-    this.enabled = true;
-  }
 };
 
-var Tabs = new Control({
-  options: {
-    generators: new Tab ({
-      id: 0,
-      name: "Generators",
-      menu: Generators,
-      enabled: true,
-    }),
-    hero: new Tab ({
-      id: 1,
-      name: "Hero",
-      enabled: true
-    }),
-    crafting: new Tab({
-      id: 2,
-      name: "Crafting",
-      enabled: true
-    }),
-    camp: new Tab({
-      id: 3,
-      name: "Camp",
-      enabled: true
-    }),
-    help: new Tab({
-      id: 4,
-      name: "Help",
-      enabled: true
-    }),
-    settings: new Tab({
-      id: 5,
-      name: "Settings",
-      enabled: true
-    })
-  },
-
-  name: "Tabs",
-  elType: "<menu />",
-  optionType: "<li />",
-  curr: 0
-});
-
-function Control (params) {
-  this.name = params.name;
-  this.location = params.location;
-  this.elType = params.elType;
-  this.optionType = params.optionType;
-  this.curr = params.curr;
-  this.options = params.options;
-}
-// Control represents one major portion of the page
-// render: renders the current element into the page, not returning anything
-// currOption: returns the option that should be currently displayed
-Control.prototype = {
-  render: function() {
-    for (option in this.options) {
-      var o = this.options[option];
-      if (o.enabled) {
-        o.render();
-      }
-    }
-  },
-
-  update: function() {
-    for (option in this.options) {
-      var o = this.options[option];
-      if (o.enabled) {
-      }
-    }
-  },
-
-  currOption: function() {
-    for (option in this.options) {
-      if (this.options[option].id == this.curr) {
-        return this.options[option];
-      }
-    }
-
-    return null;
+var ControlsData = {
+  tab: {
+    name: "Tabs",
+    elType: "menu",
+    curr: 0,
+    data: TabsData
   }
 };
 
 var Controls = {
-  nav: new Control(Tabs)
-  // Logs, Mods, etc.
+  name: "controls",
+  elType: "body",
+  data: ControlsData
 };
 
-var Engine = {
+function Control (params, parent) {
+  // All Controls
+  this.name = params.name;
+  this.parent = parent;
+
+  // Most Controls
+  this.elType = params.elType;
+  this.data = params.data;
+
+  // Menu Controls
+  this.curr = params.curr;
+}
+Control.prototype = {
+  init: function() {
+    for (key in this.data) {
+      if (this.data[key].init) {
+        this[key] = new Control(this.data[key], this);
+        this[key].init;
+      }
+    }
+  }
+  //
+  //
+  //
+  //
+  //
+  // render: function() {
+  //
+  //
+  //   for (key in this.data) {
+  //     var o = this.data[key];
+  //     if (o.enabled) {
+  //       o.render();
+  //     }
+  //   }
+  // },
+  //
+  // update: function() {
+  //   for (option in this.options) {
+  //     var o = this.options[option];
+  //     if (o.enabled) {
+  //     }
+  //   }
+  // },
+  //
+  // currOption: function() {
+  //   for (option in this.options) {
+  //     if (this.options[option].id == this.curr) {
+  //       return this.options[option];
+  //     }
+  //   }
+  //
+  //   return null;
+  // }
+};
+
+
+var Pixpls = {
   tickLength: 1000,
   tick: 0,
+  controls: Controls,
 
   init: function() {
-    this.render();
+    Controls = new Control(Controls);
+
+    Controls.init();
+
     setInterval(this.loop, this.tickLength);
   },
 
   loop: function() {
-    Engine.tick++;
-    $("#ticknumber").html("Tick #" + Engine.tick);
-    Engine.update();
+    Pixpls.tick++;
+    $("#ticknumber").html("Tick #" + Pixpls.tick);
+    Pixpls.update();
   },
 
   render: function() {
@@ -261,7 +284,7 @@ var Engine = {
 }
 
 $(document).ready(function() {
-  Engine.init();
+  Pixpls.init();
 });
 
 
