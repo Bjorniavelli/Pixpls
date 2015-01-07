8ad0cd03b508130d5100a50721f7f4ccf37668e6
Bjorniavelli <games@bjorniavelli.com>
2015-01-06 18:00:27 -0600

    Add an update loop (does nothing, yet), and render Articles.  Needs to have more encapsulation, because at the moment, every change is requiring looking at all the code.

--- Pixpls.js
+++ Pixpls.js
@@ -7,8 +7,32 @@ function Generator (params) {//name, costRatio, produceTarget, produce) {
   this.el = $("<li />");
 }
 Generator.prototype = {
+  parent: Generators,
+
   render: function() {
-    this.el.html("<p>" + this.name + ": " + this.num + "</p>");
+    this.renderArticle();
+    this.renderMenu();
+  },
+
+  renderMenu: function() {
+    var t = this;
+
+    this.el.html("<p><a href=\"\">" + this.name + "</a>: " + this.num + "</p>");
+    this.el.find("p a").click(function(e) {
+      e.preventDefault();
+
+      //This so totally breaks encapsulation... >_>
+      $("article").replaceWith(t.articleEl);
+    });
+  },
+
+  renderArticle: function() {
+    this.articleEl = $("<article />");
+    this.articleEl.append("<h1>" + this.name + "</h1>");
+    this.articleEl.append("<h2>Owned: " + this.num + "</h2>");
+    // Need to update this so that it outputs the current production rate per this.num
+    this.articleEl.append("<h3>Produces " + (this.produce || "nothing") +
+      (this.produce && this.produceTarget ? (" " + Generators[this.produceTarget].name) : "") + "</h3>");
   }
 }
 var Generators = {
@@ -18,50 +42,50 @@ var Generators = {
   pixel: new Generator({
     name: "Pixel",
     costRatio: 1.1,
-    produceTarget: 0,
+    produceTarget: "click",
     produce: -1
   }),
   renderer: new Generator({
     name: "Renderer",
     num: 0,
     costRatio: 2,
-    produceTarget: 1,
+    produceTarget: "pixel",
     produce: 1
   }),
   extruder: new Generator({
     name: "Extruder",
     costRatio: 2,
-    produceTarget: 2,
+    produceTarget: "renderer",
     produce: 1
   }),
   electronicskit: new Generator({
     name: "Electronics Kit",
     costRatio: 2,
-    produceTarget: 3,
+    produceTarget: "extruder",
     produce: 1
   }),
   factory: new Generator({
     name: "Factory",
     costRatio: 2,
-    produceTarget: 4,
+    produceTarget: "electronicskit",
     produce: 1
   }),
   cementprinter: new Generator({
     name: "Cement Printer",
     costRatio: 2,
-    produceTarget: 5,
+    produceTarget: "factory",
     produce: 1
   }),
   designlab: new Generator({
     name: "Design Lab",
     costRatio: 2,
-    produceTarget: 6,
+    produceTarget: "cementprinter",
     produce: 1
   }),
   ai: new Generator({
     name: "AI",
     costRatio: 2,
-    produceTarget: 7,
+    produceTarget: "designlab",
     produce: 1
   })
 }
@@ -76,24 +100,45 @@ function Tab (params) {
 // render should render the menu option and... create a section that may or may not be displayed?
 // renderMenu render's each item and then adds it to the list
 Tab.prototype = {
-  render: function(optionEl) {
-    this.optionEl = optionEl.html("<a href=\"\">" + this.name + "</a>");
-    this.sectionEl = $("<section />");
-
-    this.menuEl = $("<menu />");
-    this.renderMenu(this.menuEl);
-    this.sectionEl.append(this.menuEl);
-
-    this.emptyEl = $("<article />");
-    this.sectionEl.append(this.emptyEl);
+  render: function() {
+    this.renderNav();
+    this.renderSection();
   },
 
-  renderMenu: function(menuEl) {
+  renderNav: function() {
+    var t = this;
+    this.navEl = $("<li />");
+    this.navEl.html("<a href=\"\">" + this.name + "</a>");
+    this.navEl.click(function(e) {
+      e.preventDefault();
+      $("section").replaceWith(t.sectionEl);
+    });
+    $("nav").append(this.navEl);
+  },
+
+  renderSection: function() {
+    this.sectionEl = $("<section />");
+
+    this.renderMenu();
+    this.sectionEl.append(this.menuEl);
+
+    this.emptyArticleEl = $("<article />");
+    this.articleEl = this.emptyArticleEl;
+    this.sectionEl.append(this.articleEl);
+  },
+
+  renderMenu: function() {
+    this.menuEl = $("<menu />");
+
     for (menuItem in this.menu) {
       var m = this.menu[menuItem];
       m.render();
-      menuEl.append(m.el);
+      this.menuEl.append(m.el);
     }
+  },
+
+  enable: function() {
+    this.enabled = true;
   }
 };
 
@@ -133,7 +178,6 @@ var Tabs = new Control({
   },
 
   name: "Tabs",
-  location: "nav",
   elType: "<menu />",
   optionType: "<li />",
   curr: 0
@@ -152,44 +196,153 @@ function Control (params) {
 // currOption: returns the option that should be currently displayed
 Control.prototype = {
   render: function() {
-    this.el = $(this.elType);
-
     for (option in this.options) {
       var o = this.options[option];
       if (o.enabled) {
-        var optionEl = $(this.optionType);
-        o.render(optionEl);
-        this.el.append(optionEl);
+        o.render();
       }
     }
-    $(this.location).html(this.el);
+  },
+
+  update: function() {
+    for (option in this.options) {
+      var o = this.options[option];
+      if (o.enabled) {
+      }
+    }
   },
 
   currOption: function() {
     for (option in this.options) {
-      if (this.options[option].id = this.curr) {
+      if (this.options[option].id == this.curr) {
         return this.options[option];
       }
     }
 
     return null;
   }
-}
+};
 
 var Controls = {
   nav: new Control(Tabs)
   // Logs, Mods, etc.
+};
+
+var Engine = {
+  tickLength: 1000,
+  tick: 0,
+
+  init: function() {
+    this.render();
+    setInterval(this.loop, this.tickLength);
+  },
+
+  loop: function() {
+    Engine.tick++;
+    $("#ticknumber").html("Tick #" + Engine.tick);
+    Engine.update();
+  },
+
+  render: function() {
+    for (control in Controls) {
+      if (Controls[control].render) {
+        Controls[control].render();
+      }
+    }
+  },
+
+  update: function() {
+    for (control in Controls) {
+      if (Controls[control].update) {
+        Controls[control].update();
+      }
+    }
+  }
 }
 
 $(document).ready(function() {
-  $("header").append("<p>Success!</p>");
-
-  for (control in Controls) {
-    if (Controls[control].render) {
-      Controls[control].render();
-    }
-  }
-
-  //Just to test...
-  $("section").replaceWith(Controls.nav.options.generators.sectionEl);
+  Engine.init();
 });
+
+
+/*var upgrades = [
+  {
+    name: "Test Upgrade",
+    label: "Useless Upgrade",
+    description: "Just checking to see if the upgrade display is working.",
+    bought: false,
+    buy: function () { this.bought = true; app.newMessage("Bought a useless upgrade!  Go you!"); }
+  },
+  {
+    name: "Check Clicks",
+    label: "Debug Inventory Kit",
+    description: "Add 10,000,000 clicks.",
+    bought: false,
+    buy: function() { tabs[0].items[0].num += 10000000; }
+  },
+  {
+    name: "Click Preparation",
+    label: "Click Chef",
+    description: "Clicks are eating too much!  You're starving!  But maybe if you hired a chef, you'd be able to reduce your raw click consumption.",
+    cost: { medium: tabs[0].items[1], num: 1 },
+    bought: false,
+    buy: function() {
+      this.bought = true;
+      if (tabs[0].items[1].produce < 0) tabs[0].items[1].produce /= 10;
+    }
+  },
+  {
+    name: "Click Preparation II",
+    label: "Click Sous Chef",
+    description: "Your chef is obviously overworked.  Maybe you should hire someone to assist him.",
+    cost: { medium: tabs[0].items[1], num: 10 },
+    bought: false,
+    buy: function() {
+      this.bought = true;
+      if (tabs[0].items[1].produce < 0) tabs[0].items[1].produce /= 10;
+    }
+  },
+  {
+    name: "Click Preparation III",
+    label: "Click Prep Chef",
+    description: "Just two people can't provide for all your Pixpls!  You better hire some to prepare the clicks for your chef staff.",
+    cost: { medium: tabs[0].items[1], num: 100 },
+    bought: false,
+    buy: function() {
+      this.bought = true;
+      if (tabs[0].items[1].produce < 0) tabs[0].items[1].produce /= 10;
+    }
+  },
+  {
+    name: "Pixel Farming",
+    label: "Pixel Farming",
+    description: "Pixels can farm their own clicks, now!",
+    cost: { medium: tabs[0].items[0], num: 1000 },
+    bought: false,
+    buy: function() {
+      this.bought = true;
+      if (tabs[0].items[1].produce <= 0) tabs[0].items[1].produce = 0.001;
+    }
+  },
+  {
+    name: "Churn Reduction",
+    label: "Make a Space",
+    description: "It sure is annoying trying to keep track of those flickering buttons.  If we dedicated some space to them, that might be nice.",
+    bought: false,
+    buy: function() {
+      /// ??? This is the part I need to figure out.
+      // It's going to affect the ng-show of this, and I'll need to add upgrades to stuff.
+    }
+  },
+  {
+    name: "Display Generator Menu",
+    label: "",
+    description: "What's this menu for?",
+  },
+  {
+    name: "Display Hero Menu",
+    label: "",
+    description: "More than one menu?!  That's suggestive!",
+  }
+];
+*/
